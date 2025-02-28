41055a57d018ce160244af973580cd079092f762
JuanRossi2000 <89548936+JuanRossi2000@users.noreply.github.com>
2025-02-28 01:43:25 -0300

    Se actualiza encabezado

--- 06_reportes-XML.sql
+++ 06_reportes-XML.sql
@@ -1,3 +1,16 @@
+/*
+Parte cumplida: Entrega numero 4, generacion de los reportes XML solicitados
+fecha de entrega: 28/02/25
+Comisión: 1353
+Número de grupo: 3
+Materia: Bases de datos Aplicadas
+Nombres y DNI: 
+-Bautista Rios Di Gaeta, 46431397
+-Samuel Gallardo, 45926613
+-Juan Ignacio Rossi, 42115962
+-Joel Fabián Stivala Patiño, 42825990
+*/
+
 USE Com1353G03
 GO
 --1)
