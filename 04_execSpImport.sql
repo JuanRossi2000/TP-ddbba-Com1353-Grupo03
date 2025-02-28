4f57a36eefaae63395f4dec0f01f1ed5570b373d
Ferpilibauti <ferpilibauti@gmail.com>
2025-02-28 01:47:45 -0300

    Ejecución de los SP
    No hay mucho que explicar...                                                 ;)

---
+++ 04_execSpImport.sql
@@ -0,0 +1,24 @@
+/*
+Parte cumplida: Ejecucion de SP de Importe
+fecha de entrega: 28/02/25
+Comisión: 1353
+Número de grupo: 3
+Materia: Bases de datos Aplicadas
+Nombres y DNI: 
+-Bautista Rios Di Gaeta, 46431397
+-Samuel Gallardo, 45926613
+-Juan Ignacio Rossi, 42115962
+-Joel Fabián Stivala Patiño, 42825990
+*/
+
+USE Com1353G03
+
+exec ImportarInfoComplementaria 'RUTA DE ARCHIVO'
+
+exec ventas.importCatalogo 'RUTA DE ARCHIVO'
+
+exec ventas.importProductos 'RUTA DE ARCHIVO'
+
+exec ventas.importElectronic 'RUTA DE ARCHIVO'
+
+exec CargarVentas 'RUTA DE ARCHIVO'
