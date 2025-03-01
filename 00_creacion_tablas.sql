14491ddf4a82760e5030a2d30a499275c45479b4
Ferpilibauti <ferpilibauti@gmail.com>
2025-02-28 23:07:13 -0300

    MAJOR UPDATE TABLAS
    Añadido de checks, cambios de variables y eliminado de variables

--- 00_creacion_tablas.sql
+++ 00_creacion_tablas.sql
@@ -1,14 +1,14 @@
 /*
 Parte cumplida: Creacion Bd, esquemas, tablas e indices
 fecha de entrega: 27/02/25
-ComisiÃ³n: 1353
-NÃºmero de grupo: 3
+Comisión: 1353
+Número de grupo: 3
 Materia: Bases de datos Aplicadas
 Nombres y DNI: 
 -Bautista Rios Di Gaeta, 46431397
 -Samuel Gallardo, 45926613
 -Juan Ignacio Rossi, 42115962
--Joel FabiÃ¡n Stivala PatiÃ±o, 42825990
+-Joel Fabián Stivala Patiño, 42825990
 */
 IF NOT EXISTS(	SELECT 1 FROM SYS.DATABASES WHERE name = 'Com1353G03')
 BEGIN
@@ -73,16 +73,18 @@ BEGIN
 		direccion VARCHAR(255),
 		emailPersonal VARCHAR(255),
 		emailEmpresa VARCHAR(255) NOT NULL,
-		cuil bigint NOT NULL,
+		cuil char(11) NOT NULL,
 		cargo VARCHAR(30) NOT NULL,
 		sucursalId int NOT NULL,
 		turno VARCHAR(17) NOT NULL,
 		habilitado bit DEFAULT 1,
-		FOREIGN KEY (sucursalID) REFERENCES rrhh.sucursal(id)
+		FOREIGN KEY (sucursalID) REFERENCES rrhh.sucursal(id),
+		CONSTRAINT CHK_turno CHECK (turno IN ('TM', 'TT','Jornada Completa')),
+		CONSTRAINT CHK_dni CHECK (dni > 0),
+		CONSTRAINT CHK_cargo CHECK (cargo IN ('Cajero', 'Supervisor', 'Gerente de sucursal'))
 	);
 END;
 GO
-
 IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'ventas' AND TABLE_NAME = 'Cliente')
 BEGIN
 	CREATE TABLE ventas.Cliente(
@@ -152,14 +154,18 @@ BEGIN
 		empleadoID int NOT NULL,
 		pagoID int NOT NULL,
 		estadoId int NOT NULL,
-		precioTotal decimal(11,2),
 		identificadorPago varchar(25),
+		sucursalId int NOT NULL,
 		habilitado BIT DEFAULT 1,
 		FOREIGN KEY (estadoId) REFERENCES ventas.Estado(id),
 		FOREIGN KEY (empleadoID) REFERENCES rrhh.Empleado(legajo),
 		FOREIGN KEY (pagoID) REFERENCES ventas.MedioPago(id),
+		FOREIGN KEY (sucursalId) REFERENCES rrhh.Sucursal(id),
 		CONSTRAINT CHK_genero_factura CHECK (genero = 'Male' OR genero = 'Female'),
+		CONSTRAINT CHK_tipo_factura CHECK (tipo = 'A' OR tipo = 'B' OR tipo = 'C'),
+		CONSTRAINT CHK_tipo_ciente CHECK (tipoCliente = 'Member' OR tipoCliente = 'Normal'),
 		CONSTRAINT CHK_nro CHECK (nro LIKE '[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]')
+
 	);
 END;
 GO
@@ -186,10 +192,14 @@ BEGIN
 	CREATE TABLE ventas.NotaCredito (
 		id INT IDENTITY(1,1) PRIMARY KEY,
 		facturaID INT NOT NULL,
+		empleadoId INT NOT NULL,
 		fecha DATETIME DEFAULT GETDATE(),
 		monto DECIMAL(10,2) NOT NULL,
-		tipoNota CHAR(1) NOT NULL,  -- 'D' para devoluciÃ³n, 'S' para sustituciÃ³n
-		FOREIGN KEY (facturaID) REFERENCES ventas.Factura(id)
+		tipoNota CHAR(1) NOT NULL,  -- 'D' para devolución, 'S' para sustitución
+		FOREIGN KEY (empleadoId) REFERENCES rrhh.Empleado(legajo),
+		FOREIGN KEY (facturaID) REFERENCES ventas.Factura(id),
+		CONSTRAINT CHK_TipoNota CHECK (tipoNota = 'D' OR tipoNota = 'S'),
+		CONSTRAINT CHK_montoNota CHECK (monto > 0)
 	);
 END;
 GO
@@ -198,4 +208,4 @@ IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'nix_nroFactura' AND objec
 BEGIN
     CREATE NONCLUSTERED INDEX nix_nroFactura 
     ON ventas.Factura(nro);
-END
+END;
