45e7c3bcdab3559cfe0d739b79b8183c9f09cdfc
JoelStivala <126842578+JoelStivala@users.noreply.github.com>
2025-03-03 11:03:11 -0300

    Actualizacion 04_execSpImport.sql
    Se agrega un valor por defecto a la tabla Moneda

--- 04_execSpImport.sql
+++ 04_execSpImport.sql
@@ -1,18 +1,20 @@
 /*
 Parte cumplida: Ejecucion de SP de Importe
 fecha de entrega: 28/02/25
-Comisión: 1353
-Número de grupo: 3
+ComisiÃ³n: 1353
+NÃºmero de grupo: 3
 Materia: Bases de datos Aplicadas
 Nombres y DNI: 
 -Bautista Rios Di Gaeta, 46431397
 -Samuel Gallardo, 45926613
 -Juan Ignacio Rossi, 42115962
--Joel Fabián Stivala Patiño, 42825990
+-Joel FabiÃ¡n Stivala PatiÃ±o, 42825990
 */
 
 USE Com1353G03
 
+exec utilidades.altaMoneda 'USD', 1230 
+  
 exec ImportarInfoComplementaria 'RUTA DE ARCHIVO'
 
 exec ventas.importCatalogo 'RUTA DE ARCHIVO'
