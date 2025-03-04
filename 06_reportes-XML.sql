852c5e958187740e2ddeec45bd03996be1d21136
samuu1i <elitete23153@gmail.com>
2025-03-04 20:22:06 -0300

    Modificacion fecha

--- 06_reportes-XML.sql
+++ 06_reportes-XML.sql
@@ -1,6 +1,6 @@
 /*
 Parte cumplida: Generacion de los reportes XML solicitados
-fecha de entrega: 28/02/25
+fecha de entrega: 04/03/25
 Comisión: 1353
 Número de grupo: 3
 Materia: Bases de datos Aplicadas
